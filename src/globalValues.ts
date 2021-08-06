eecf1cbea2bce05f923ba0fb9e55ec2fd3c1f704
Lei Lu <41553112+a362654886@users.noreply.github.com>
2021-08-06 23:04:06 +1200

    update

--- src/globalValues.ts
+++ src/globalValues.ts
@@ -1,2 +1,2 @@
-export const backEndLink = "https://4e3a8b5r77.execute-api.us-east-1.amazonaws.com/dev/"
+export const backEndLink = "https://y033h2hpb6.execute-api.us-east-1.amazonaws.com/dev/"
 //export const backEndLink = "http://localhost:3000/dev/"
